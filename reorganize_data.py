ad511acd2f8ef536ccd40f0f77cf06ab1818d2a1
chaseez <chasezundel@gmail.com>
2024-02-29 15:13:32 -0700

    Updated reorgainze_data.py to work on a freshly extracted data file

--- reorganize_data.py
+++ reorganize_data.py
@@ -18,26 +18,30 @@ for file in pathlib.Path(images_path).iterdir():
 
     if 'test' in file.name:
         for data in pathlib.Path(file).iterdir():
-            if 'image-datasets' == data.name: continue
+            # Only iterate over the class directories
+            if 'image-datasets' == data.name or 'input' in data.name: continue
 
             if pathlib.Path(f'{test_path}/{data.name}').exists():
+                # print(data.name)
                 for f in pathlib.Path(data).iterdir():
                     pathlib.Path(f).rename(f'{test_path}/{data.name}/{f.name}')
 
             else:
                 pathlib.Path(data).rename(f'{test_path}/{data.name}')
-            print(f'{test_path}/{data.name}')
+            # print(f'{test_path}/{data.name}')
     else:
         for data in pathlib.Path(file).iterdir():
-            if 'image-datasets' == data.name: continue
+            # Only iterate over the class directories
+            if 'image-datasets' == data.name or 'input' in data.name: continue
 
             if pathlib.Path(f'{train_path}/{data.name}').exists():
+
                 for f in pathlib.Path(data).iterdir():
                     pathlib.Path(f).rename(f'{train_path}/{data.name}/{f.name}')
 
             else:
                 pathlib.Path(data).rename(f'{train_path}/{data.name}')
-            print(f'{train_path}/{data.name}')
+            # print(f'{train_path}/{data.name}')
 
     # Only iterate through the test and train files
     # for f in pathlib.Path(file).iterdir():
